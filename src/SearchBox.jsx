47576306a8e0d9d7de98ef6152183db1142df5f9
shubham sharma <shubhamsharma585@gmail.com>
2023-04-13 01:16:42 +0530

    f3

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "./useData";
 
 function SearchBox() {
@@ -6,30 +6,99 @@ function SearchBox() {
     "http://www.mocky.io/v2/5ba8efb23100007200c2750c"
   );
   const [searchText, setSearchText] = useState("");
+  const [relatedResults, setRelatedResults] = useState([]);
   console.log(data);
+
+  useEffect(() => {
+    const searchFields = ["address", "id", "items", "name", "pincode"];
+    const searchRegex = new RegExp(searchText, "i");
+
+    let ndata = data.filter((obj) =>
+      searchFields.some((field) => obj[field].toString().match(searchRegex))
+    );
+    setRelatedResults(ndata);
+  }, [searchText, data]);
+
+  const highlightText = (text, search) => {
+    const regex = new RegExp(`(${search})`, "gi");
+    return text.replace(regex, "<mark>$1</mark>");
+  };
+
+  const checkInItems = (query, arr) => {
+    let flag = arr?.some((itm) =>
+      itm.toLowerCase().includes(query.toLowerCase())
+    );
+    return flag;
+  };
 
   return (
     <div>
       <h4>{"Search Box"}</h4>
       {error ? <p>{error}</p> : null}
-      {isLoading ? "Loading..." : null}
-      <input
-        value={searchText}
-        onChange={(e) => setSearchText(e.target.value)}
-      />
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <input
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          placeholder="Search users by ID, name and others"
+        />
+      )}
       <div
         style={{
           border: "4px solid blue",
           width: "50%",
           margin: "auto",
           marginTop: 40,
+          height: 500,
+          overflow: "scroll",
         }}
       >
-        {data?.map((item) => (
-          <div key={item.name} style={{ border: "3px solid black" }}>
-            {item.name}
+        {relatedResults?.length > 0 ? (
+          <>
+            {relatedResults?.map((item) => (
+              <div key={item.id} style={{ border: "3px solid black" }}>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: highlightText(item.id, searchText),
+                  }}
+                />
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: highlightText(item.name, searchText),
+                  }}
+                />
+                <p>
+                  {searchText !== "" ? (
+                    checkInItems(searchText, item.items) ? (
+                      <>
+                        <span>{`·`}</span>
+                        <span style={{ color: "blue", marginRight: 10 }}>
+                          {searchText}
+                        </span>
+                        <span>{`found in items`}</span>
+                      </>
+                    ) : null
+                  ) : null}
+                </p>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: highlightText(item.address, searchText),
+                  }}
+                />
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: highlightText(item.pincode, searchText),
+                  }}
+                />
+              </div>
+            ))}
+          </>
+        ) : (
+          <div>
+            <h4>{"No User Found"}</h4>
           </div>
-        ))}
+        )}
       </div>
     </div>
   );
